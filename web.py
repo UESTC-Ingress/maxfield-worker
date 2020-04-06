be54c802880fe6194bb2d655de209d54a532d581
Charles Yang <Charles@0x7f.cc>
2020-04-06 19:02:36 +0800

    Bug fix.

--- web.py
+++ web.py
@@ -3,6 +3,10 @@ import os.path
 
 app = Flask(__name__)
 
+@app.route('/')
+def index_page():
+    return 'It works!'
+
 @app.route('/<taskid>/<filename>')
 def get_task_files(taskid, filename):
     if os.path.isfile('/tmp/maxfield-worker-results/' + taskid + '/' + filename):
@@ -11,4 +15,4 @@ def get_task_files(taskid, filename):
         return '404 Not Found'
 
 if __name__ == '__main__':
-    app.run(port=1331)
+    app.run(port=1331,host="0.0.0.0")
