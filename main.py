b4f8b8948c86e69672b542c2d5aead9e29779dfc
Charles Yang <Charles@0x7f.cc>
2020-04-07 13:56:32 +0800

    Add faction.

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def do_max_field(reqbody):
         f.write(reqbody["portal"])
     try:
         maxfield.maxfield("/tmp/maxfield.tmp.txt",
-                          int(reqbody["agents"]), num_cpus=0, output_csv=True, outdir="/tmp/maxfield-worker")
+                          int(reqbody["agents"]), res_colors=(reqbody["faction"] == "res"), num_cpus=0, output_csv=True, outdir="/tmp/maxfield-worker")
         json_object = json.dumps({
             "agents": int(reqbody["agents"])
         })
