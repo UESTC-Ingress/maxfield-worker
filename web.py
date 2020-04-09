c31098abce77a0476ae06d92e3bf8e6b2b3fac88
Charles Yang <Charles@0x7f.cc>
2020-04-09 10:39:10 +0800

    Add cores.

--- web.py
+++ web.py
@@ -2,14 +2,23 @@ from flask import Flask, send_file
 from flask_cors import CORS, cross_origin
 import os.path
 
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+
 app = Flask(__name__)
 
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+
 @app.route('/')
 def index_page():
-    return 'It works!'
+    return {
+        "msg": "It works!",
+        "cores": os.environ.get("RBQPass")
+    }
+
 
 @app.route('/<taskid>/<filename>')
 @cross_origin()
@@ -19,5 +28,6 @@ def get_task_files(taskid, filename):
     else:
         return '404 Not Found'
 
+
 if __name__ == '__main__':
-    app.run(port=1331,host="0.0.0.0")
+    app.run(port=1331, host="0.0.0.0")
