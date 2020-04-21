f88c6d59387ffdba65c73253cb0d81402d244dc2
Charles Yang <Charles@0x7f.cc>
2020-04-21 17:57:10 +0800

    Fix bug.

--- docker-entry.sh
+++ docker-entry.sh
@@ -2,5 +2,8 @@
 
 set -e
 
+hostname worker
+
 exec python /code/web.py &
+
 exec python /code/main.py
--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import time
 import json
 
 from dotenv import find_dotenv, load_dotenv
-from threading import Timer
 
 import maxfield.maxfield.maxfield as maxfield
 
@@ -25,11 +24,14 @@ def delete_old_dir():
     for _dir in os.listdir(path):
         if os.path.getmtime(path + '/' + _dir) < old:
             print("[MaxFieldWorker] Deleted expired ID "+_dir)
-            os.remove(path + '/' + _dir)
+            shutil.rmtree(path + '/' + _dir)
 
 
 def check_dir():
     if not os.path.exists("/tmp/maxfield-worker"):
+        os.mkdir("/tmp/maxfield-worker")
+    else:
+        shutil.rmtree("/tmp/maxfield-worker")
         os.mkdir("/tmp/maxfield-worker")
     if not os.path.exists("/tmp/maxfield-worker-results"):
         os.mkdir("/tmp/maxfield-worker-results")
@@ -46,7 +48,15 @@ def callback(ch, method, properties, body):
     connection.close()
     check_dir()
     print("[MaxFieldWorker] Received a new request.")
-    result = do_max_field(json.loads(str(body, encoding="utf-8")))
+    req_body = json.loads(str(body, encoding="utf-8"))
+    with open('/tmp/maxfield-runinfo', 'w') as f:
+        json.dump({
+            'req_body': req_body,
+            "routing_key": properties.reply_to,
+            "correlation_id": properties.correlation_id
+        }, f)
+    result = do_max_field(req_body)
+    os.remove('/tmp/maxfield-runinfo')
     if result:
         shutil.move("/tmp/maxfield-worker",
                     "/tmp/maxfield-worker-results/" + properties.correlation_id)
@@ -68,6 +78,7 @@ def do_max_field(reqbody):
         if(reqbody.get("googlemap", False)):
             google_api_key = os.environ.get("GoogleMapAPIKey")
             google_api_secret = os.environ.get('GoogleMapAPISecret')
+        print("[MaxFieldWorker] Job Started.")
         maxfield.maxfield("/tmp/maxfield.tmp.txt",
                           int(reqbody["agents"]), google_api_key=google_api_key, google_api_secret=google_api_secret, res_colors=(reqbody["faction"] == "res"), num_cpus=int(os.environ.get("CORES")), output_csv=True, outdir="/tmp/maxfield-worker")
         json_object = json.dumps({
@@ -81,7 +92,7 @@ def do_max_field(reqbody):
         return False
 
 
-def start_loop(sendack=False, senddata=None):
+def start_loop(sendack=False, senddata=None, recover=False):
     global connection
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(host=os.environ.get("RBQHost"), virtual_host=os.environ.get("RBQBase"), credentials=credentials))
@@ -91,16 +102,34 @@ def start_loop(sendack=False, senddata=None):
     channel.basic_qos(prefetch_count=1)
     channel.basic_consume(
         queue='maxfield-task', consumer_tag=os.environ.get("NODEName")+":"+os.environ.get("NODEURL"), on_message_callback=callback, auto_ack=True)
-    if sendack:
+    print('[MaxFieldWorker] Service is now up.')
+    if recover:
+        print("[MaxFieldWorker] Recovered from previous request.")
+        with open('/tmp/maxfield-runinfo', 'r') as f:
+            rec_info = json.load(f)
+            result = do_max_field(rec_info['req_body'])
+            if result:
+                shutil.move("/tmp/maxfield-worker",
+                            "/tmp/maxfield-worker-results/" + rec_info['correlation_id'])
+            print("[MaxFieldWorker] Job Completed.")
+            senddata = {
+                "status": result,
+                "correlation_id": rec_info['correlation_id'],
+                "routing_key":  rec_info['routing_key']
+            }
+            os.remove('/tmp/maxfield-runinfo')
+            delete_old_dir()
+    if sendack or recover:
         channel.basic_publish(
             exchange='',
             body=json.dumps({
                 "node": os.environ.get("NODEName"),
                 "status": senddata["status"]
             }), routing_key=senddata["routing_key"], properties=pika.BasicProperties(correlation_id=senddata["correlation_id"]))
-    print('[MaxFieldWorker] Service is now up.')
     channel.start_consuming()
 
 
 if __name__ == "__main__":
+    if os.path.exists('/tmp/maxfield-runinfo'):
+        start_loop(recover=True)
     start_loop()
