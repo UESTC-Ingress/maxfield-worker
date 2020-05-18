239cc3a11dac05d8fc50629cdb89981756a34971
Charles Yang <Charles@0x7f.cc>
2020-05-19 02:11:11 +0800

    Another bug fix.

--- reporter.py
+++ reporter.py
@@ -13,17 +13,20 @@ credentials = pika.PlainCredentials(
     os.environ.get('RBQUser'), os.environ.get("RBQPass"))
 
 connection = None
+channel = None
 
 SAVEPATH = '/tmp/maxfield-worker-results'
 
 def init_ch():
     global connection
+    global channel
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(host=os.environ.get("RBQHost"), virtual_host=os.environ.get("RBQBase"), credentials=credentials))
     channel = connection.channel()
     channel.queue_declare(queue='maxfield-task', durable=True)
 
 def start_loop():
+    global channel
     for _dir in os.listdir(SAVEPATH):
         if _dir.endswith(".json"):
             print("[MaxFieldWorker] Process result " + _dir)
